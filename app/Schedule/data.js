e6640dbf2cd38f2813d11b8c6ee3626007384b4d
Peter Gira <49673257+Pistoja@users.noreply.github.com>
2019-11-26 17:56:59 +0100

    add plugin nativescript-ui-listview

--- app/Schedule/data.js
+++ app/Schedule/data.js
@@ -5,14 +5,16 @@ module.exports = [
             "inizio": "8:00",
             "fine": "9:00",
             "luogo": "Caffè degli specchi",
-            "id": "162gs38dh"
+            "id": "162gs38dh",
+            "group": "8:00 - 10:00"
           },
           {
             "nome": "Presentazione ESOF 2019",
             "inizio": "9:00",
             "fine": "10:00",
             "luogo": "Piazza Unità d'Italia",
-            "id": "@263gs79"
+            "id": "@263gs79",
+            "group": "8:00 - 10:00"
           },
 
           {
@@ -20,7 +22,8 @@ module.exports = [
             "inizio": "10:00",
             "fine": "11:00",
             "luogo": "Piazza Unità d'Italia",
-            "id": "@263gs79"
+            "id": "@263gs79",
+            "group": "10:00 - 11:00"
           }
 
         ]
