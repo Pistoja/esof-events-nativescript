6bcc08e3f23b59cfbb8e79876ed70728f40b8e72
Peter Gira <49673257+Pistoja@users.noreply.github.com>
2019-12-02 17:07:19 +0100

    speakers by data

--- app/Speaker/Speaker-view-model.js
+++ app/Speaker/Speaker-view-model.js
@@ -1,9 +1,13 @@
 const observableModule = require("tns-core-modules/data/observable");
+const sdata = require('./sdata')
+const sredata = sdata.map((item) => {
+    item.interventocompleto =`${item.nome} - ${item.inizio} ${item.fine}`
+    return item
+});
 
 function SpeakerViewModel() {
     const viewModel = observableModule.fromObject({
-        /* Add your view model properties here */
-        test: `ciao mondo`
+        items: sredata
     });
 
     return viewModel;
