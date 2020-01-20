5276248790f9503d675e4a1a8d8185738936cd77
Peter Gira <49673257+Pistoja@users.noreply.github.com>
2020-01-20 16:46:51 +0100

    Login (incomplete)

--- app/Schedule/Schedule-items-page.js
+++ app/Schedule/Schedule-items-page.js
@@ -1,39 +1,73 @@
 const firebase = require("nativescript-plugin-firebase");
 const HomeItemsViewModel = require("./Schedule-items-view-model");
 
-// firebase.login({
-//     type: firebase.LoginType.PASSWORD,
-//     passwordOptions: {
-//         email: 'webnicola@gmail.com',
-//         password: 'theirpassword'
-//     }
-// })
-// .then(result => {
-//     console.log('OK', result)
-//     alert({
-//         title: 'Autetenticazione OK',
-//         message: `Ciao, ${result.email}`,
-//         okButtonText: 'OK'
-//     })
-// })
-// .catch(error => console.error(error))
+ firebase.login({
+     type: firebase.LoginType.PASSWORD,
+     passwordOptions: {
+         email: 'petergira95@gmail.com',
+         password: 'theirpassword'
+     }
+ })
+ .then(result => {
+     console.log('OK', result)
+     alert({
+         title: 'Autetenticazione OK',
+         message: `Ciao, ${result.email}`,
+         okButtonText: 'OK'
+     })
+ })
+ .catch(error => console.error(error))
 
 // firebase.logout()
 // .then(() => console.log('LOGOUT'))
 // .catch(error => console.error(error))
-
-// firebase.sendPasswordResetEmail('webnicola@gmail.com')
-// .then(() => console.log('Password reset by email'))
-// .catch(error => console.error('error', error))
-
+/*    // cambiare password
+ firebase.sendPasswordResetEmail('petergira95@gmail.com')
+ .then(() => console.log('Password reset by email'))
+ .catch(error => console.error('error', error))
+*/
 firebase.getCurrentUser()
-.then(user => console.log('**user**', user))
-.catch(error => console.error('No user find', error))
+.then(user => console.log('getCurrentUser', user))
+.catch(error => console.error('getCurrentUser', error))
 
 function onNavigatingTo(args) {
     const component = args.object;
     component.bindingContext = new HomeItemsViewModel();
 }
 
+function showDialog (){
+    console.log(`login`);
+    dialogs.login({
+        title: `Login`,
+        message: `Enter credential`,
+        okButtonText: "Go",
+        cancelButtonText: "Cancel",
+
+        userName: "User",
+        password: "Password"
+    }).then(function(r){
+        console.log("Dialog result: " + r.result + ", user: " + r.userName + ", pwd: " + r.password)
+        if (r.result){
+            firebase.login({
+                type: firebase.LoginType.PASSWORD,
+                passwordOptions: {
+                    email: r.userName,
+                    password: r.password
+                }
+            })
+            .then(result => {
+                console.log(`OK`, result)
+                alert({
+                    title: `Autenticazione OK`,
+                    message: `Ciao, ${result.email}`,
+                    okButtonText: `OK`
+                })
+
+            })
+        }
+    });
+}
+
 
 exports.onNavigatingTo = onNavigatingTo;
+exports.showDialog = this.showDialog
